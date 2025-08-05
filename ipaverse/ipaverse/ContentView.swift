9c8ca15e83dd6adc8bb1062effeae29ff06c65b5
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2025-08-06 01:14:52 +0300

    Created xcode project

---
+++ ipaverse/ipaverse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ipaverse
+//
+//  Created by BAHATTIN KOC on 6.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, ipaverse!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ipaverse/ipaverse/ipaverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipaverseApp.swift
+//  ipaverse
+//
+//  Created by BAHATTIN KOC on 6.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ipaverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
